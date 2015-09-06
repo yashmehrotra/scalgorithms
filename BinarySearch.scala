968ea756ef0f30cddce01508129a4ed0499e6f35
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-09-06 13:00:39 +0530

    Binary Search Working

--- BinarySearch.scala
+++ BinarySearch.scala
@@ -2,21 +2,25 @@ object BinarySearch {
     def main(args: Array[String]) {
       var arr = List(1, 2, 3, 7, 20, 300, 641)
       var num = 3
-      searchNum(num, 0, arr.length, arr)
+      searchNum(num, 0, 6, arr)
     }
 
-    def searchNum(num: Int, start: Int, end: Int, arr: List[Int]): Boolean = {
-      if (arr(start) == num) {
+    def searchNum(num: Int, start: Int, end: Int, arr: List[Int]): Int = {
+      var mid = start + ((end - 1)/2)
+
+      if (start >= end) {
+        println("Doesnt Exists")
+        return -1
+      }
+      if (arr(mid) == num) {
         println("Number Exists")
-        return true
-      } else if (start >= end) {
-        println("Number doesnt exist")
-        println(start)
-        println(end)
-        return false
-      }else {
-        searchNum(num,start,end/2,arr)
-        searchNum(num,end/2+1,end,arr)
+        return mid
+      } 
+
+     if (arr(mid) > num) {
+        return searchNum(num, mid+1, end, arr)
+      } else {
+        return searchNum(num,start,mid-1,arr)
       }
     }
 }
